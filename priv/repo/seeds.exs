68944a0b89fbe39cc0e8d93c40947077d858784e
Jack Marchant <jack@expert360.com>
2018-04-26 23:48:40 +1000

    update seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,45 +1,21 @@
-alias Contento.Content
+alias Contento.{Settings, Themes, Accounts}
 
 now = fn -> DateTime.utc_now() end
 
-Content.create_page(%{
-  "slug" => "about",
-
-  "title" => "About",
-  "content" => "Tell people something nice about you!",
-
-  "published" => true,
-  "published_at" => now.(),
-
-  "author_id" => 1
-})
-
-Content.create_post(%{
-  "slug" => "getting-started",
-
-  "title" => "Getting started with Contento",
-  "content" => "Hello!! This is your first post on Contento!",
-
-  "published" => true,
-  "published_at" => now.(),
-
-  "author_id" => 1
-})
-
-Contento.Accounts.create_user(%{
+Accounts.create_user(%{
   "name" => "Default User",
   "handle" => "contento",
   "email" => "contento@example.org",
   "password" => "contento"
 })
 
-Contento.Settings.create_settings(%{
+Settings.create_settings(%{
   "website_title" => "An Awesome Website",
   "website_description" => "Random Opinions and Ideas",
   "theme_id" => 1
 })
 
-Contento.Themes.create_theme(%{
+Themes.create_theme(%{
   "id" => 1,
   "alias" => "simplo",
   "name" => "Simplo",
