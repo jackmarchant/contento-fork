1a15faac846616386a6506e0be3705001d356de7
Jack Marchant <jack@expert360.com>
2018-04-26 23:44:57 +1000

    run post deploy and setup seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -25,3 +25,28 @@ Content.create_post(%{
 
   "author_id" => 1
 })
+
+Contento.Accounts.create_user(%{
+  "name" => "Default User",
+  "handle" => "contento",
+  "email" => "contento@example.org",
+  "password" => "contento"
+})
+
+Contento.Settings.create_settings(%{
+  "website_title" => "An Awesome Website",
+  "website_description" => "Random Opinions and Ideas",
+  "theme_id" => 1
+})
+
+Contento.Themes.create_theme(%{
+  "id" => 1,
+  "alias" => "simplo",
+  "name" => "Simplo",
+  "description" => "Simple theme",
+  "inserted_at" => now.(),
+  "updated_at" => now.(),
+  "setting_id" => 1,
+})
+
+
